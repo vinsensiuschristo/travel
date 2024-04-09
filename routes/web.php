f6035443191de269990615a22f559a1814264af8
vinsensiuschristo <hallavinsen@gmail.com>
2024-04-09 22:09:51 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about-us', function () {
+    return view('aboutus.index');
+});
