7b9301021bd241f595d47490a5aafcfa58206f7d
vinsensiuschristo <hallavinsen@gmail.com>
2024-11-13 14:17:58 +0700

    add URL in register AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
